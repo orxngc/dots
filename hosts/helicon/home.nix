db439ce927caafa694f5ef45d24500ee88db8f70
orxngc <orangc@proton.me>
2024-06-11 11:40:58 -0400

    move hypridle thing to home instead of config

--- hosts/helicon/home.nix
+++ hosts/helicon/home.nix
@@ -368,6 +368,33 @@ in {
     })
   ];
 
+  services = {
+    hypridle = {
+      settings = {
+        general = {
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+          ignore_dbus_inhibit = false;
+          lock_cmd = "hyprlock";
+          starship = {
+            enable = true;
+            package = pkgs.starship;
+          };
+        };
+        listener = [
+          {
+            timeout = 900;
+            on-timeout = "hyprlock";
+          }
+          {
+            timeout = 1200;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+        ];
+      };
+    };
+  };
+
   programs = {
     gh.enable = true;
     neovim = {
