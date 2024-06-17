3b3654b180b3a2ff4d3532d25c71a79d734d5b10
orxngc <orangc@proton.me>
2024-06-17 13:15:47 -0400

    update walls script

--- scripts/walls.nix
+++ scripts/walls.nix
@@ -1,6 +1,8 @@
 { pkgs }:
 pkgs.writeShellScriptBin "wallset" ''
   cd ~/Pictures/walls-catppuccin-mocha
+  swww kill
+  swww-daemon
   while : ; do
      export CURRENT_WALLPAPER="$(ls *.jpg *.png *.jpeg | sort -R | tail -1)"
      swww img "$CURRENT_WALLPAPER" --transition-type grow --transition-duration 0.75 --transition-fps 60
