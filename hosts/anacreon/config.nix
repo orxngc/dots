7665112babe27a87bbde979d405532525c0dc78f
orxngc <orangc@proton.me>
2024-06-18 19:25:05 -0400

    update anacreon

--- hosts/anacreon/config.nix
+++ hosts/anacreon/config.nix
@@ -49,6 +49,55 @@
     plymouth.enable = true;
   };
 
+  # Styling Options
+  # stylix = {
+  #   enable = true;
+  #   image = ../../config/face.png;
+  #   base16Scheme = {
+  #     base00 = "1e1e2e";
+  #     base01 = "181825";
+  #     base02 = "313244";
+  #     base03 = "45475a";
+  #     base04 = "585b70";
+  #     base05 = "cdd6f4";
+  #     base06 = "f5e0dc";
+  #     base07 = "b4befe";
+  #     base08 = "f38ba8";
+  #     base09 = "fab387";
+  #     base0A = "f9e2af";
+  #     base0B = "a6e3a1";
+  #     base0C = "94e2d5";
+  #     base0D = "89b4fa";
+  #     base0E = "cba6f7";
+  #     base0F = "f2cdcd";
+  #   };
+  #   polarity = "dark";
+  #   opacity.terminal = 0.8;
+  #   cursor.package = pkgs.bibata-cursors;
+  #   cursor.name = "Bibata-Modern-Ice";
+  #   cursor.size = 24;
+  #   fonts = {
+  #     monospace = {
+  #       package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+  #       name = "JetBrainsMono Nerd Font Mono";
+  #     };
+  #     sansSerif = {
+  #       package = pkgs.montserrat;
+  #       name = "Montserrat";
+  #     };
+  #     serif = {
+  #       package = pkgs.montserrat;
+  #       name = "Montserrat";
+  #     };
+  #     sizes = {
+  #       applications = 12;
+  #       terminal = 15;
+  #       desktop = 11;
+  #       popups = 12;
+  #     };
+  #   };
+  # };
+
   # Extra Module Options
   drivers.amdgpu.enable = true;
   drivers.nvidia.enable = false;
@@ -88,7 +137,6 @@
   programs = {
     hyprland = {
       enable = true;
-      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
       xwayland.enable = true;
     };
     firefox.enable = true;
@@ -297,8 +345,6 @@
       alsa.support32Bit = true;
       pulse.enable = true;
     };
-    rpcbind.enable = true;
-    nfs.server.enable = true;
   };
   hardware.sane = {
     enable = true;
