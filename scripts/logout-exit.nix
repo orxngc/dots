52652aeb77f4e3aeb15b78fa0aaf8f7503ab4dba
orxngc <orangc@proton.me>
2024-07-04 23:05:56 -0400

    new wlogout style

--- scripts/logout-exit.nix
+++ scripts/logout-exit.nix
@@ -1,8 +1,7 @@
-{ pkgs, host }:
+{ pkgs }:
 let
-inherit
-    (import ../hosts/${host}/variables.nix) wlogout_horizontal;
+inherit (import ../hosts/helicon/variables.nix) wlogout_horizontal;
 in
 pkgs.writeShellScriptBin "logout-exit" ''
-  killall logout; wlogout --css ~/.config/wlogout/${if wlogout_horizontal then ''style_horizontal.css'' else ''style.css''} --layout ~/.config/wlogout/${if wlogout_horizontal then ''layout_horizontal'' else ''layout -b 2 -L 600 -R 600''} 
+  killall logout; wlogout --css ~/.config/wlogout/${if wlogout_horizontal then ''style_horizontal.css'' else ''style.css''} --layout ~/.config/wlogout/${if wlogout_horizontal then ''layout_horizontal -b 5 -T 400 -B 400'' else ''layout -b 2 -L 600 -R 600''} 
 ''
