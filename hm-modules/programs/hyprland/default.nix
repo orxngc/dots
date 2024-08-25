0129a28fc6f85d9755261989b8dcb07719690b4e
orxngc <orangc@proton.me>
2024-08-25 07:26:34 +0300

    fix major bug with hyprland not sourcing its config properly

--- hm-modules/programs/hyprland/default.nix
+++ hm-modules/programs/hyprland/default.nix
@@ -1,25 +1,25 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: let
+{ config
+, lib
+, pkgs
+, ...
+}:
+let
   inherit (lib) mkOption types;
 in
-  with lib; {
-    options.hmModules.programs.hyprland = mkOption {
-      enabled = mkOption {
-        type = types.bool;
-        default = true;
-      };
+with lib; {
+  options.hmModules.programs.hyprland = mkOption {
+    enabled = mkOption {
+      type = types.bool;
+      default = true;
     };
-    config = {
-      wayland.windowManager.hyprland = {
-        enable = true;
-        systemd.enable = true;
-        extraConfig = concatStrings [
-          ''source = ~/dots/hm-modules/core/hyprland/hyprland.conf''
-        ];
-      };
+  };
+  config = {
+    wayland.windowManager.hyprland = {
+      enable = true;
+      systemd.enable = true;
+      extraConfig = concatStrings [
+        ''source = ~/dots/hm-modules/programs/hyprland/hyprland.conf''
+      ];
     };
-  }
+  };
+}
